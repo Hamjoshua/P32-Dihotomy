1838d033133437e287a3b403c2c6c19a968567f1
Hamjoshua <biryukov-10@list.ru>
2024-09-15 20:32:27 +0700

    Добавлен интерфейс для методов

--- PalMathy/Methods/BaseMethod.cs
+++ PalMathy/Methods/BaseMethod.cs
@@ -15,9 +15,50 @@ namespace PalMathy.Methods
         List<DataPoint> Points;
         string FunctionString;
 
-        double BeginInterval;
-        double EndInterval;
+        double BeginInterval = 0;
+        double EndInterval = 10;
         
+        void CalculateGraph()
+        {
+            var medianLine = new LineSeries
+            {
+                Title = "X",
+                Color = OxyColor.FromRgb(255, 0, 0), // Красный цвет
+                StrokeThickness = 2
+            };
+
+            medianLine.Points.Add(new DataPoint(-10, 0));
+            medianLine.Points.Add(new DataPoint(10, 0));
+
+            var absicc = new LineSeries
+            {
+                Title = "Y",
+                Color = OxyColor.FromRgb(255, 0, 0), // Красный цвет
+                StrokeThickness = 2,
+            };
+
+            absicc.Points.Add(new DataPoint(0, 10));
+            absicc.Points.Add(new DataPoint(0, -10));
+
+
+            // Создаем серию точек графика
+            var lineSeries = new LineSeries
+            {
+                Title = "f(x)",
+                Color = OxyColor.FromRgb(0, 0, 255), // Синий цвет линии
+                LineStyle = LineStyle.Dot
+            };
+
+            // Добавляем все точки в серию
+            lineSeries.Points.AddRange(Points);
+
+            // Добавляем серию точек к модели графика
+            Graph.Series.Add(lineSeries);
+            Graph.Series.Add(medianLine);
+            Graph.Series.Add(absicc);
+
+        }
+
         void ParseFunction()
         {
             Function parsedFunction = new Function("f(x) = " + FunctionString);
---
+++ PalMathy/Pages/DihotomyPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PalMathy.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DihotomyPage.xaml
+    /// </summary>
+    public partial class DihotomyPage : Page
+    {
+        public DihotomyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
