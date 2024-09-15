22af45e7c5701c3af71e861f0803bcb91de855b6
Hamjoshua <biryukov-10@list.ru>
2024-09-15 20:00:20 +0700

    Начата разработка интерфейсов MVVM

---
+++ PalMathy/Methods/BaseMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.mariuszgromada.math.mxparser;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace PalMathy.Methods
+{
+    class BaseMethod
+    {
+        PlotModel Graph = new PlotModel { Title = "График" };
+        List<DataPoint> Points;
+        string FunctionString;
+
+        double BeginInterval;
+        double EndInterval;
+        
+        void ParseFunction()
+        {
+            Function parsedFunction = new Function("f(x) = " + FunctionString);
+
+            for (double counterI = BeginInterval; counterI <= EndInterval; ++counterI)
+            {
+                Expression e1 = new Expression($"f({counterI})", parsedFunction);
+                Points.Add(new DataPoint(counterI, e1.calculate()));
+            }
+
+        }
+
+    }
+}
