018b829c17aad23e80edc88fe66e77e2d468f030
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2024-09-19 15:07:33 +0700

    рефакторинг и фикс

---
+++ PalMathy/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PalMathy.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        protected void Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+
+            if (EqualityComparer<T>.Default.Equals(field, newValue)) return;
+            field = newValue;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
