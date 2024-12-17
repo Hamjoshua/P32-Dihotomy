30873235d382ddf77907f159b90d5dd31bbaba70
Hamjoshua <biryukov-10@list.ru>
2024-12-17 10:46:19 +0700

    Графическое представление матрицы

--- PalMathy/ViewModels/SlauViewModel.cs
+++ PalMathy/ViewModels/SlauViewModel.cs
@@ -11,17 +11,29 @@ namespace PalMathy.ViewModels
     public class SlauViewModel : BaseViewModel
     {
         BaseLinearEquation _method = new GaussEquation();
+        private List<List<int>> _matrix = new List<List<int>>() {
+                        new List<int> { 3, 2, -5, -1 },
+                        new List<int> { 2, -1, 3, 13 },
+                        new List<int> { 1, 2, -1, 9 }};
+        public List<List<int>> Matrix
+        {
+            get
+            {
+                return _matrix;
+            }
+            set
+            {
+                _matrix = value;
+                OnPropertyChanged(nameof(Matrix));
+            }
+        }
         public ICommand GetNumbers
         {
             get
             {
                 return new DelegateCommand((obj) =>
                 {
-                    List<List<int>> data = new List<List<int>>() {
-                        new List<int> { 3, 2, -5, -1 },
-                        new List<int> { 2, -1, 3, 13 },
-                        new List<int> { 1, 2, -1, 9 }};
-                    _method.GetNumbers(data);
+                    _method.GetNumbers(Matrix);
                 });
             }
         }
